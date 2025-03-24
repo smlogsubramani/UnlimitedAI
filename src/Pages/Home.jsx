e5a969f49d654fc514f90b9c9dae8a9473b9b011
smlogsubramani <smlogasubramani@gmail.com>
2025-03-24 15:16:16 +0530

    Service has been changed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -25,7 +25,42 @@ const Home = () =>{
                 <p className="service-subcontent" >Unifying Intelligence, Amplifying Potential—Your Gateway to Multiple AI features.</p>
             </div>
             <div id="servicecontainers" className="row">
-                <div className="col">
+            <div class="accordion accordion-flush" id="accordionFlushExample">
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="flush-headingOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                    Agentic AI
+                    </button>
+                  </h2>
+                  <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">Agentic AI bridges the gap between advanced technology & human needs empowering individual and business</div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="flush-headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                    Autonomus Agent
+                    </button>
+                  </h2>
+                  <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">Autonomous Agents are self-directed systems designed to perform tasks, make decisions without humans</div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="flush-headingThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                    Predictive Models
+                    </button>
+                  </h2>
+                  <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">Predictive models analyze historical data to forecast future outcomes, enabling smarter decisions and proactive strategies.</div>
+                  </div>
+                </div>
+            </div>
+            </div>
+
+      
+                {/* <div className="col">
                     <div className="cardcontainer">
                         <h1 className="card-number">01</h1>
                         <h1 className="cardheader">Agentic AI</h1>
@@ -48,11 +83,13 @@ const Home = () =>{
                     <p className="cardpara">Predictive models analyze historical data to forecast future outcomes, enabling smarter decisions and proactive strategies.</p>
                     <button className="cardbtn">Read More</button>
                     </div>
-                </div>
-            </div>
+                </div> */}
+ 
             </div>
         </div>
     )
 }
+
+
 
 export default Home;
